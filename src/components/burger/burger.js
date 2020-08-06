b64af0af8fc5034a7b8e2b51807a30d39996e3f6
e1bandito <aleksandr.shitov@mail.ru>
2020-08-06 20:58:43 +0300

    Update 2020-08-06T17:58:36.186Z

--- src/components/burger/burger.js
+++
@@ -1,8 +0,0 @@
-import $ from "jquery";
-
-$('.burger').on('click', function () {
-  $(this).toggleClass('burger--open');
-  $('.nav').toggleClass('nav--open');
-
-  // $('body').toggleClass('body-lock');
-});
